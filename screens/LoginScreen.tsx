1861b61f631bfcce042caa768ae9263e654dc559
BakangMonei <bakangmonei2@gmail.com>
2024-05-21 04:03:03 +0200

    Wroking, now adding pictures

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import React from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 
 const LoginScreen: React.FC = ({ navigation }) => {
+
+
+    const handleSubmission = async () => {
+        // Handle form submission
+        navigation.navigate("Main");
+    }
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Login</Text>
-      
+
       <TextInput
         placeholder="Email"
         placeholderTextColor="#666"
@@ -19,15 +31,15 @@ const LoginScreen: React.FC = ({ navigation }) => {
         secureTextEntry
       />
 
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={handleSubmission}>
         <Text style={styles.buttonText}>Login</Text>
       </TouchableOpacity>
-      
-      <TouchableOpacity onPress={() => navigation.navigate('ForgotPassword')}>
+
+      <TouchableOpacity onPress={() => navigation.navigate("ForgotPassword")}>
         <Text style={styles.link}>Forgot Password?</Text>
       </TouchableOpacity>
-      
-      <TouchableOpacity onPress={() => navigation.navigate('RegistrationPage')}>
+
+      <TouchableOpacity onPress={() => navigation.navigate("RegistrationPage")}>
         <Text style={styles.link}>Don't have an account? Sign Up</Text>
       </TouchableOpacity>
     </View>
@@ -37,20 +49,20 @@ const LoginScreen: React.FC = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
     padding: 20,
-    backgroundColor: '#ffffff',
+    backgroundColor: "#ffffff",
   },
   title: {
     fontSize: 32,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20,
-    textAlign: 'center',
-    color: '#333333',
+    textAlign: "center",
+    color: "#333333",
   },
   input: {
     height: 50,
-    borderColor: '#cccccc',
+    borderColor: "#cccccc",
     borderWidth: 1,
     borderRadius: 8,
     paddingHorizontal: 10,
@@ -58,21 +70,21 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   button: {
-    backgroundColor: '#ff6347',
+    backgroundColor: "#ff6347",
     height: 50,
     borderRadius: 8,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginBottom: 10,
   },
   buttonText: {
-    color: '#ffffff',
+    color: "#ffffff",
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   link: {
-    color: '#ff6347',
-    textAlign: 'center',
+    color: "#ff6347",
+    textAlign: "center",
     marginTop: 10,
   },
 });
