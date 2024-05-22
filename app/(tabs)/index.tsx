070e434525050ef2fb01d2759a3b82e40066edf3
BakangMonei <bakangmonei2@gmail.com>
2024-05-22 02:41:45 +0200

    Added State Management

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,13 +7,14 @@ import { ThemedView } from "@/components/ThemedView";
 import { NavigationContainer } from "@react-navigation/native";
 // import MainNavigator from "./../../navigation/MainNavigator";
 import AppNavigator from "@/navigation/AppNavigator";
+import { AppProvider } from '@/statemanagement/AppContext';
 
 export default function HomeScreen() {
   return (
-    // <NavigationContainer>
+    <AppProvider>
       <AppNavigator />
-    // {/* </NavigationContainer> */}
+    </AppProvider>
   );
-}
+};
 
 
--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -1,44 +1,57 @@
-import React from "react";
+// AppNavigator.tsx
+import React, { useContext } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import SplashScreen from "../screens/SplashScreen";
-import MainNavigator from "./MainNavigator";
+import SplashScreen from "@/screens/SplashScreen";
+import MainNavigator from "./MainNavigator"; // You may need to adjust this import based on your folder structure
 import LoginScreen from "@/screens/LoginScreen";
 import RegistrationScreen from "@/screens/RegistrationScreen";
 import ForgotPasswordScreen from "@/screens/ForgotPasswordScreen";
+import { AppContext } from "@/statemanagement/AppContext";
 
 const Stack = createNativeStackNavigator();
 
-const AppNavigator = () => (
-  <Stack.Navigator initialRouteName="Splash">
-    <Stack.Screen
-      name="Splash"
-      component={SplashScreen}
-      options={{ headerShown: false }}
-    />
-    <Stack.Screen
-      name="Main"
-      component={MainNavigator}
-      options={{ headerShown: false }}
-    />
+const AppNavigator: React.FC = () => {
+  const { state } = useContext(AppContext);
 
-    <Stack.Screen
-      name="LoginPage"
-      component={LoginScreen}
-      options={{ headerShown: false }}
-    />
-
-    <Stack.Screen
-      name="ForgotPassword"
-      component={ForgotPasswordScreen}
-      options={{ headerShown: false }}
-    />
-
-    <Stack.Screen
-      name="RegistrationPage"
-      component={RegistrationScreen}
-      options={{ headerShown: false }}
-    />
-  </Stack.Navigator>
-);
+  return (
+    <Stack.Navigator initialRouteName="Splash">
+      <Stack.Screen
+        name="Splash"
+        component={SplashScreen}
+        options={{ headerShown: false }}
+      />
+      {state.isLoggedIn ? (
+        <Stack.Screen
+          name="MainNavigator" // Change this to "MainNavigator" or any other appropriate name
+          component={MainNavigator}
+          options={{ headerShown: false }}
+        />
+      ) : (
+        <>
+          <Stack.Screen
+            name="LoginPage"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="ForgotPassword"
+            component={ForgotPasswordScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="RegistrationPage"
+            component={RegistrationScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Main"
+            component={MainNavigator}
+            options={{ headerShown: false }}
+          />
+        </>
+      )}
+    </Stack.Navigator>
+  );
+};
 
 export default AppNavigator;
